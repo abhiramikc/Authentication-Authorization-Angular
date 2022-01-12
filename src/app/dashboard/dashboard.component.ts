536e544c36198dc7a799d78dcbf38fbf22499e75
Gittin Baby_AB <gittin.baby@claysys.net>
2022-01-12 12:26:26 +0530

    changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,19 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
 
+  constructor(private http:HttpClient) {
+
+  }
+data: any = {}
   ngOnInit() {
+    this.getData();
+  }
+
+  getData()
+  {
     this.http.get("https://localhost:44308/api/Customer/cu",{
       headers:new HttpHeaders({
         "Content-Type": "application/json"
       })
     }).subscribe(response => {
       console.log("response from api ",response);
+      this.data=response;
+
+
     },
     err => {
       console.log("error from api ",err);
     });
   }
-
+  public logOut=() =>{
+    localStorage.removeItem("jwt");
+  }
 }
